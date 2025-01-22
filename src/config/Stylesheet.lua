c5182cb79825432111200dc77a1bc61ba79f025c
TDK <43817240+MoneySnyder@users.noreply.github.com>
2025-01-22 16:53:21 -0600

    new: initial setup

---
+++ src/config/Stylesheet.lua
@@ -0,0 +1,6 @@
+return {
+    Surface1Color = Color3.fromRGB(0, 0, 0),
+    Surface2Color = Color3.fromRGB(197, 197, 197),
+    PrimaryTextColor = Color3.fromRGB(255, 255, 255),
+    ShadowColor = Color3.fromRGB(0, 0, 0),
+}
